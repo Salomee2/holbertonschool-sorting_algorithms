3139ebaf83e13c20e611c66dbbac3dca2fca66be
Faf-dev <noaillesjulien@gmail.com>
2024-12-12 15:17:06 +0100

    add file.c file

---
+++ file.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * insertion_sort_list - insertion sort algorithm
+ * @list: head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *position = *list;
+	listint_t *temp;
+}
