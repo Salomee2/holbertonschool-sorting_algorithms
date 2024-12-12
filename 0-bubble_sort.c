888778a3e42f0456220efbda6cb5e1d75ecfe961
Faf-dev <noaillesjulien@gmail.com>
2024-12-12 15:03:38 +0100

    attempt task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,19 +12,21 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL)
 		return;
-	for (size_t i = 0; i < size; i++)
+	for (size_t i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-			for (j = 0; j < size - i; j++)
+			for (j = 0; j < size - i - 1; j++)
 			{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[j - 1];
-				array[j - 1] = array[j];
-				array[j] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				swapped += 1;
 				print_array(array, size);
 			}
 		}
+		if (swapped == 0)
+			break;
 	}
 }
