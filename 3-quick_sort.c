b685ea5a417cadb993aa4ccb1165f1d3952f70e2
root <pertonsalomee@gmail.com>
2024-12-13 12:23:27 +0000

    3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,78 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * swap - Swap two elements in an array
+ * @a: First element
+ * @b: Second element
+ */
+void swap(int *a, int *b)
+{
+	if (a != b)
+	{
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+}
+
+/**
+ * lomuto_partition - Lomuto partition scheme
+ * @array: Array to partition
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Total size of the array
+ * 
+ * Return: Index of the pivot after partition
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	print_array(array, size);
+	return (i + 1);
+}
+
+/**
+ * quick_sort_rec - Recursive quick sort implementation
+ * @array: Array to sort
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Total size of the array
+ */
+void quick_sort_rec(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot_index = lomuto_partition(array, low, high, size);
+
+		quick_sort_rec(array, low, pivot_index - 1, size);
+		quick_sort_rec(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sort an array using the Quick sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quick_sort_rec(array, 0, size - 1, size);
+}
+
