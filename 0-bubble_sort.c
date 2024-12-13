68281b9644d7fb04c33a6de828fb619b67b281c8
root <pertonsalomee@gmail.com>
2024-12-13 12:47:01 +0000

    fix 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,15 +8,17 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t temp, swapped, j;
+	size_t i, j, swapped;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
-	for (size_t i = 0; i < size - 1; i++)
+
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-			for (j = 0; j < size - i - 1; j++)
-			{
+		for (j = 0; j < size - i - 1; j++)
+		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
@@ -30,3 +32,4 @@ void bubble_sort(int *array, size_t size)
 			break;
 	}
 }
+
